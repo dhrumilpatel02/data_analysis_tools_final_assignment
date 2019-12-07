8aa576244e6475aa0544d52b0787d7d462fbf6e4
Dhrumil Patel <dhrumilpatel02@gmail.com>
2019-12-06 21:19:30 -0500

    Imported Dataset and previewed the first five rows
    Imported Dataset and made two different dataframes to run analysis on two different variables

--- analysis.py
+++ analysis.py
@@ -2,3 +2,11 @@
 import pandas as pd
 import matplotlib as plt
 %matplotlib inline
+
+# Read CSV and store in two different dataframes
+df = pd.read_csv('survey.csv')
+fd = pd.read_csv('survey.csv')
+
+# Print first five values to see the whole dataset
+print(df.head())
+print(fd.head())
