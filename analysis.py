b41f902d8e980373de86e364fb10e48fb14c0e41
Dhrumil Patel <dhrumilpatel02@gmail.com>
2019-12-06 21:20:42 -0500

    Rename values
    Rename seek_help and treatment's Categorical values to Numerical values for smoother filtering

--- analysis.py
+++ analysis.py
@@ -10,3 +10,10 @@ fd = pd.read_csv('survey.csv')
 # Print first five values to see the whole dataset
 print(df.head())
 print(fd.head())
+
+# Rename seek_help and treatment's Categorical values to Numerical values for smoother filtering
+df["seek_help"] = df["seek_help"].replace(["Yes", "Don't know", "No"], [1, 0.5, 0])
+df = df[df.seek_help==1]
+
+fd["treatment"] = fd["treatment"].replace(["Yes", "No"], [1, 0]) 
+fd = fd[fd.treatment==1]
